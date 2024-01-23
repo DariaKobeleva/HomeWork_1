4c194d0eb1326731b4abcb9c01f955429fcd8110
KDaria <dkdva@mail.ru>
2024-01-23 12:17:00 +0100

    Initial Commit

---
+++ HomeWork_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_1
+//
+//  Created by Дарья Кобелева on 23.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
